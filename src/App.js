21b54e7d581650a8c35705a4b0ab29e6cf555186
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2025-12-02 13:13:18 +0530

    Implement form state management and item submission in Form component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import { useState } from "react";
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -24,19 +24,41 @@ function Logo() {
   );
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault(); // to prevent reloading the page
-    console.log('Submitted');
+
+    if (!description) return;
+
+    const newItem = { description, quantity, id: Date.now(), packed: false };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <>
-      <form className="add-form" onSubmit={handleSubmit}>What do you need for your trip?
-        <select>
+      <form className="add-form" onSubmit={handleSubmit}>
+        What do you need for your trip?
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
           {/* To generate number list can be used this Array.from */}
-          {Array.from({length:20},(_,i)=>i+1).map(num =><option value={num} key={num}>{num}</option>)}
+          {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          ))}
         </select>
-        <input type="text" placeholder="Item..."/>
+        <input
+          type="text"
+          placeholder="Item..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
         <button>Add</button>
       </form>
     </>
@@ -52,13 +74,13 @@ function PackingList() {
       </ul>
       ;
     </div>
-  ); 
+  );
 }
 
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? {textDecoration:'line-through'} : {}}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
       <button>❌</button>
